f747ce0f93da996b04bba27b3aa7e620dc6da48a
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-09-09 16:43:41 -0700

    -Added comments for data save types. -Added if statements for every save option

--- SaveExtraShipData/SaveExtraShipData.cs
+++ SaveExtraShipData/SaveExtraShipData.cs
@@ -31,7 +31,7 @@ namespace SaveExtraShipData
         public static bool SaveShipAlertLevel = true;
         public static bool SaveWarpTargets = true;
         public static bool SaveCrewAllowance = true;
-        public static bool SaveCommandState = true;
+        public static bool SaveCaptainOrder = true;
 
         public override string Identifier()
         {
@@ -52,103 +52,163 @@ namespace SaveExtraShipData
                 {
                     writer.Write(SavePerFile);                  //bool SavePerFile
                     writer.Write(SaveAutoTargeting);            //bool SaveAutoTargeting
-                    writer.Write(playerShip.AutoTarget);
+                    if (SaveAutoTargeting)
+                    {
+                        writer.Write(playerShip.AutoTarget);    //bool TutoTarget
+                    }
                     writer.Write(SaveShieldStatus);             //bool SaveShieldStatus
-                    writer.Write(playerShip.ShieldFreqMode);
+                    if (SaveShieldStatus)
+                    {
+                        writer.Write(playerShip.ShieldFreqMode);//int ShieldFrequency
+                    }
                     writer.Write(SaveShieldIntegrity);          //bool SaveShieldIntegrity
-                    writer.Write(playerShip.MyStats.ShieldsCurrent);
+                    if (SaveShieldIntegrity)
+                    {
+                        writer.Write(playerShip.MyStats.ShieldsCurrent); //float ShieldIntegrity
+                    }
                     writer.Write(SaveReactorPowerSettings);     //bool SaveReactorPowerSettings
-                    writer.Write(playerShip.SystemPowerLevels[0]);
-                    writer.Write(playerShip.SystemPowerLevels[1]);
-                    writer.Write(playerShip.SystemPowerLevels[2]);
-                    writer.Write(playerShip.SystemPowerLevels[3]);
-                    writer.Write(playerShip.ReactorTotalPowerLimitPercent);
+                    if (SaveReactorPowerSettings)
+                    {
+                        writer.Write(playerShip.SystemPowerLevels[0]); //float
+                        writer.Write(playerShip.SystemPowerLevels[1]); //float
+                        writer.Write(playerShip.SystemPowerLevels[2]); //float
+                        writer.Write(playerShip.SystemPowerLevels[3]); //float
+                        writer.Write(playerShip.ReactorTotalPowerLimitPercent); //float
+                    }
                     writer.Write(SaveReactorSafetyToggle);      //bool SaveReactorSafetyToggle
-                    writer.Write(playerShip.ReactorCoolingEnabled);
+                    if (SaveReactorSafetyToggle)
+                    {
+                        writer.Write(playerShip.ReactorCoolingEnabled); //bool
+                    }
                     writer.Write(SaveAuxReactor);               //bool SaveAuxReactor
-                    writer.Write(playerShip.AuxConfig);
+                    if (SaveAuxReactor)
+                    {
+                        writer.Write(playerShip.AuxConfig);     //byte
+                    }
                     writer.Write(SaveFuelCellLoaded);           //bool SaveFuelCellLoaded
-                    writer.Write(playerShip.WarpCapsuleIsLoaded);
+                    if (SaveFuelCellLoaded)
+                    {
+                        writer.Write(playerShip.WarpCapsuleIsLoaded); //bool
+                    }
                     writer.Write(SaveShipPowerLevers);          //bool SaveShipPowerLevers
-                    writer.Write(playerShip.StartupSwitchBoard.GetStatus(0));
-                    writer.Write(playerShip.IsShipOSBootingUp);
-                    writer.Write(playerShip.StartupSwitchBoard.GetStatus(1));
-                    writer.Write(playerShip.CrewControlEnabled);
-                    writer.Write(playerShip.IsPrimingWarpDrive);
-                    writer.Write(playerShip.StartupSwitchBoard.GetStatus(2));
+                    if (SaveShipPowerLevers)
+                    {
+                        writer.Write(playerShip.StartupSwitchBoard.GetStatus(0)); //bool
+                        writer.Write(playerShip.IsShipOSBootingUp);               //bool
+                        writer.Write(playerShip.StartupSwitchBoard.GetStatus(1)); //bool
+                        writer.Write(playerShip.CrewControlEnabled);              //bool
+                        writer.Write(playerShip.IsPrimingWarpDrive);              //bool
+                        writer.Write(playerShip.StartupSwitchBoard.GetStatus(2)); //bool
+                    }
                     writer.Write(SaveSystemPowerSettings);      //bool SaveSystemPowerSettings
-                    writer.Write(playerShip.PowerPercent_SysIntConduits[0]);
-                    writer.Write(playerShip.PowerPercent_SysIntConduits[1]);
-                    writer.Write(playerShip.PowerPercent_SysIntConduits[2]);
-                    writer.Write(playerShip.PowerPercent_SysIntConduits[3]);
-                    writer.Write(playerShip.PowerPercent_SysIntConduits[4]);
-                    writer.Write(playerShip.PowerPercent_SysIntConduits[5]);
-                    writer.Write(playerShip.PowerPercent_SysIntConduits[6]);
-                    writer.Write(playerShip.PowerPercent_SysIntConduits[7]);
-                    writer.Write(playerShip.PowerPercent_SysIntConduits[8]);
-                    writer.Write(playerShip.PowerPercent_SysIntConduits[10]);
-                    int maxturrets = playerShip.MyStats.GetSlot(ESlotType.E_COMP_TURRET).MaxItems;
-                    for (int i = 0; i < maxturrets; i++)
+                    if (SaveSystemPowerSettings)
                     {
-                        writer.Write(playerShip.PowerPercent_SysIntConduits[11 + i]);
-                    }
-                    if (playerShip.MyStats.GetSlot(ESlotType.E_COMP_AUTO_TURRET).MaxItems > 0)
-                    {
-                        writer.Write(playerShip.PowerPercent_SysIntConduits[15]);
-                    }
-                    writer.Write(SaveSystemHealthLevels);       //bool SaveSystemHealthLevels
-                    writer.Write(playerShip.ComputerSystem.Health);
-                    writer.Write(playerShip.EngineeringSystem.Health);
-                    writer.Write(playerShip.WeaponsSystem.Health);
-                    writer.Write(playerShip.LifeSupportSystem.Health);
-                    writer.Write(SaveCloakState);               //bool SaveCloakState
-                    writer.Write(playerShip.GetIsCloakingSystemBeingPrimed());
-                    writer.Write(playerShip.CloakingSystemPrimeAmt);
-                    writer.Write(SaveNuclearDeviceStatus);      //bool SaveNuclearDeviceStatus
-                    writer.Write(playerShip.NuclearLaunchStage);
-                    if (playerShip.NuclearLaunchStage > 0)
-                    {
-                        writer.Write(playerShip.LoadedNuclearDevice.SubType);
-                        writer.Write(playerShip.LoadedNuclearDevice.Level);
-                        writer.Write(playerShip.NukeActivator.GetSwitchStatus(0));
-                        writer.Write(playerShip.NukeActivator.GetSwitchStatus(0));
-                    }
-                    writer.Write(SaveMissileLauncherSetup);     //bool SaveMissileLauncherSetup
-                    Dictionary<uint, int> items = new Dictionary<uint, int>();
-                    foreach(PLSlotItem slot in playerShip.MyStats.GetSlot(ESlotType.E_COMP_TRACKERMISSILE))
-                    {
-                        if(slot != null)
+                        writer.Write(playerShip.PowerPercent_SysIntConduits[0]); //float
+                        writer.Write(playerShip.PowerPercent_SysIntConduits[1]); //float
+                        writer.Write(playerShip.PowerPercent_SysIntConduits[2]); //float
+                        writer.Write(playerShip.PowerPercent_SysIntConduits[3]); //float
+                        writer.Write(playerShip.PowerPercent_SysIntConduits[4]); //float
+                        writer.Write(playerShip.PowerPercent_SysIntConduits[5]); //float
+                        writer.Write(playerShip.PowerPercent_SysIntConduits[6]); //float
+                        writer.Write(playerShip.PowerPercent_SysIntConduits[7]); //float
+                        writer.Write(playerShip.PowerPercent_SysIntConduits[8]); //float
+                        writer.Write(playerShip.PowerPercent_SysIntConduits[10]);//float
+                        int maxturrets = playerShip.MyStats.GetSlot(ESlotType.E_COMP_TURRET).MaxItems;
+                        for (int i = 0; i < maxturrets; i++)
                         {
-                            items.Add(slot.getHash(), ((PLTrackerMissile)slot).TargetedSystemID);
+                            writer.Write(playerShip.PowerPercent_SysIntConduits[11 + i]); //float
+                        }
+                        if (playerShip.MyStats.GetSlot(ESlotType.E_COMP_AUTO_TURRET).MaxItems > 0)
+                        {
+                            writer.Write(playerShip.PowerPercent_SysIntConduits[15]); //float
                         }
                     }
-                    writer.Write(items.Count);
-                    foreach(KeyValuePair<uint, int> item in items)
+                    writer.Write(SaveSystemHealthLevels);       //bool SaveSystemHealthLevels
+                    if (SaveSystemHealthLevels)
                     {
-                        writer.Write(item.Key);
-                        writer.Write(item.Value);
+                        writer.Write(playerShip.ComputerSystem.Health);    //float
+                        writer.Write(playerShip.EngineeringSystem.Health); //float
+                        writer.Write(playerShip.WeaponsSystem.Health);     //float
+                        writer.Write(playerShip.LifeSupportSystem.Health); //float
+                    }
+                    writer.Write(SaveCloakState);               //bool SaveCloakState
+                    if (SaveCloakState)
+                    {
+                        writer.Write(playerShip.GetIsCloakingSystemBeingPrimed()); //bool
+                        writer.Write(playerShip.CloakingSystemPrimeAmt);           //float
+                    }
+                    writer.Write(SaveNuclearDeviceStatus);      //bool SaveNuclearDeviceStatus
+                    if (SaveNuclearDeviceStatus)
+                    {
+                        writer.Write(playerShip.NuclearLaunchStage);                  //int
+                        if (playerShip.NuclearLaunchStage > 0)
+                        {
+                            writer.Write(playerShip.LoadedNuclearDevice.SubType);     //int
+                            writer.Write(playerShip.LoadedNuclearDevice.Level);       //int
+                            writer.Write(playerShip.NukeActivator.GetSwitchStatus(0));//bool
+                            writer.Write(playerShip.NukeActivator.GetSwitchStatus(0));//bool
+                        }
+                    }
+                    writer.Write(SaveMissileLauncherSetup);     //bool SaveMissileLauncherSetup
+                    if (SaveMissileLauncherSetup)
+                    {
+                        Dictionary<uint, int> items = new Dictionary<uint, int>();
+                        foreach (PLSlotItem slot in playerShip.MyStats.GetSlot(ESlotType.E_COMP_TRACKERMISSILE))
+                        {
+                            if (slot != null)
+                            {
+                                items.Add(slot.getHash(), ((PLTrackerMissile)slot).TargetedSystemID);
+                            }
+                        }
+                        writer.Write(items.Count);              //int
+                        foreach (KeyValuePair<uint, int> item in items)
+                        {
+                            writer.Write(item.Key);             //uint
+                            writer.Write(item.Value);           //int
+                        }
                     }
                     writer.Write(SaveO2Level);                  //bool Save02Level 
-                    writer.Write(playerShip.MyStats.OxygenLevel);
-                    writer.Write(SaveBiohazardLevel);           //bool SaveBiohazardLevel
-                    writer.Write(playerShip.AcidicAtmoBoostAlpha);
-                    writer.Write(SaveShipAlertLevel);           //bool SaveShipAlertLevel
-                    writer.Write(playerShip.AlertLevel);
-                    writer.Write(SaveCrewAllowance);            //bool SaveCrewAllowance
-                    writer.Write(PLServer.Instance.CurrentCrewCreditsAvailableToCrew);
-                    writer.Write(SaveWarpCharge);               //bool SaveWarpCharge
-                    writer.Write((int)playerShip.WarpChargeStage);
-                    writer.Write(playerShip.WarpChargeState_Levels[0]);
-                    writer.Write(playerShip.WarpChargeState_Levels[1]);
-                    writer.Write(playerShip.WarpChargeState_Levels[2]);
-                    writer.Write(SaveWarpTargets);              //bool SaveWarpTargets
-                    writer.Write(PLServer.Instance.m_ShipCourseGoals.Count);
-                    foreach(int coursegoal in PLServer.Instance.m_ShipCourseGoals)
+                    if (SaveO2Level)
                     {
-                        writer.Write(coursegoal);
+                        writer.Write(playerShip.MyStats.OxygenLevel);//float
                     }
-                    writer.Write(SaveCommandState);             //bool SaveCommandState
-                    writer.Write(PLServer.Instance.CaptainsOrdersID);
+                    writer.Write(SaveBiohazardLevel);           //bool SaveBiohazardLevel
+                    if (SaveBiohazardLevel)
+                    {
+                        writer.Write(playerShip.AcidicAtmoBoostAlpha);//float
+                    }
+                    writer.Write(SaveShipAlertLevel);           //bool SaveShipAlertLevel
+                    if (SaveShipAlertLevel)
+                    {
+                        writer.Write(playerShip.AlertLevel);    //int
+                    }
+                    writer.Write(SaveCrewAllowance);            //bool SaveCrewAllowance
+                    if (SaveCrewAllowance)
+                    {
+                        writer.Write(PLServer.Instance.CurrentCrewCreditsAvailableToCrew); //int
+                    }
+                    writer.Write(SaveWarpCharge);               //bool SaveWarpCharge
+                    if (SaveWarpCharge)
+                    {
+                        writer.Write((int)playerShip.WarpChargeStage);     //int
+                        writer.Write(playerShip.WarpChargeState_Levels[0]);//float
+                        writer.Write(playerShip.WarpChargeState_Levels[1]);//float
+                        writer.Write(playerShip.WarpChargeState_Levels[2]);//float
+                    }
+                    writer.Write(SaveWarpTargets);              //bool SaveWarpTargets
+                    if (SaveWarpTargets)
+                    {
+                        writer.Write(PLServer.Instance.m_ShipCourseGoals.Count);//int
+                        foreach (int coursegoal in PLServer.Instance.m_ShipCourseGoals)
+                        {
+                            writer.Write(coursegoal);           //int
+                        }
+                    }
+                    writer.Write(SaveCaptainOrder);             //bool SaveCommandState
+                    if (SaveCaptainOrder)
+                    {
+                        writer.Write(PLServer.Instance.CaptainsOrdersID);//int
+                    }
                 }
                 return myStream;
             }
